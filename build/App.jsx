2f0e445ca34eae13871a98b6b6405a48fc6cfb61
ameyabaxi <102268376+ameyabaxi@users.noreply.github.com>
2022-03-23 21:14:25 -0500

    Add files via upload

---
+++ build/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Project from "./Page/Project/Project";
+import Home from "./Page/Home/Home";
+import { BrowserRouter, Router, Route, Link, Switch } from "react-router-dom";
+import ProjectDetails from "./Page/Project/ProjectDetails";
+import About from "./Page/About/About";
+import Member from "./Page/Member/Member";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/project" component={Project} />
+        <Route path="/projects/:id" component={ProjectDetails} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/member" component={Member} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
